59d135356e26b84a8d340d23a6a6c4705be1336a
tyjuji <tyjuji@gmx.com>
2021-10-12 14:49:48 +0200

    Untested start

---
+++ MandatoryOneTCP/BooksManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MandatoryOneLibrary;
+
+namespace MandatoryOneTCP
+{
+public class BooksManager
+    {
+        private List<Book> _books = new List<Book>{ 
+            new Book {Title = "Computer Networking, Global Edition", 
+                    Author = "James Kurose, Keith Ross", 
+                    PageNumber = 800, ISBN13 = "9781292405469"},
+            new Book {Title = "Extreme Programming Explained",
+                    Author = "Kent Beck, Cynthia Andres",
+                    PageNumber = 224, ISBN13 = "9780321278654"},
+            new Book {Title = "The Agile Samurai",
+                    Author = "Jonathan Rasmusson",
+                    PageNumber = 280, ISBN13 = "9781934356586"}
+            };
+
+        public BooksManager()
+        {
+
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public Book GetByISBN(string isbn)
+        {
+            return _books.Find(x => x.ISBN13 == isbn);
+        }
+
+        public Book PostBook(Book book)
+        {
+            if(!_books.Contains(book))
+            {
+                _books.Add(book);
+            }
+
+            return book;
+        }
+
+        public Book PutBook(string ISBN, Book updates)
+        {
+            Book book = _books.Find(book1 => book1.ISBN13 == ISBN);
+            if (book == null) return null;
+            book.Title = updates.Title;
+            book.PageNumber = updates.PageNumber;
+            book.Author = updates.Author;
+            return book;
+        }
+
+        public Book DeleteBook(string isbn)
+        {
+            Book book = _books.Find(x => x.ISBN13 == isbn);
+            if (book != null)
+            {
+                _books.RemoveAll(x => x.ISBN13 == isbn);
+                return book;
+            }
+            return null;
+        }
+    }
+}
--- MandatoryOneTCP/Program.cs
+++ MandatoryOneTCP/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text.Json;
+using MandatoryOneLibrary;
 
 namespace MandatoryOneTCP
 {
@@ -6,7 +12,72 @@ namespace MandatoryOneTCP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("Server started");
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 4646);
+            listener.Start();
+
+            Console.WriteLine("Server ready");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task task = new Task(() => { HandleClient(socket); });
+                task.Start();
+                Task.Run(() => 
+                {
+                    Console.WriteLine("Waiting for input from Client.");
+                    task.Wait();
+                    if (task.IsCompletedSuccessfully){
+                        Console.WriteLine("Client disconnected successfully.");
+                    }
+                });
+            }
+
         }
+        private static void HandleClient(TcpClient socket)
+        {
+            var manager = new BooksManager();
+
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+
+            HandleRequest(reader, writer, socket);
+
+
+            void HandleRequest(StreamReader reader, StreamWriter writer, TcpClient socket)
+            {
+                string message1 = reader.ReadLine();
+                string message2 = reader.ReadLine();
+                Console.WriteLine("Received request: " + message1);
+                Console.WriteLine("Received content: " + message2);
+                
+                if(message1.Trim() == "GetAll")
+                {
+                    var result = manager.GetAll();
+                    string jsonString = JsonSerializer.Serialize(result);
+                    writer.WriteLine(jsonString);
+                    writer.Flush();
+                }
+                
+                if(message1.Trim() == "Get")
+                {
+                    var result = manager.GetByISBN(message2);
+                    string jsonString = JsonSerializer.Serialize(result);
+                    writer.WriteLine(jsonString);
+                    writer.Flush();
+                }
+
+                if(message1.Trim() == "Save")
+                {
+                    Book deserialized = JsonSerializer.Deserialize<Book>(message2);
+                    manager.PostBook(deserialized);
+                }
+            }
+        }
+        
     }
 }
