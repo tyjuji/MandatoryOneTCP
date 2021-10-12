a834fd0279b6aa8e18bbc48563e73db45aa3b1c5
tyjuji <tyjuji@gmx.com>
2021-10-12 13:45:51 +0200

    init

---
+++ MandatoryOneTCP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MandatoryOneTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
